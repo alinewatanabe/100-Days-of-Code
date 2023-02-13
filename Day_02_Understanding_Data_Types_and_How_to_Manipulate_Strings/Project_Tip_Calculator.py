3fc1fb855c16ee9924c5b1ee4abf411ef60faa0e
Aline Watanabe <linenwatanabe@gmail.com>
2023-02-13 09:51:48 -0300

    Project Complete

--- Day_02_Understanding_Data_Types_and_How_to_Manipulate_Strings/Project_Tip_Calculator.py
+++ Day_02_Understanding_Data_Types_and_How_to_Manipulate_Strings/Project_Tip_Calculator.py
@@ -5,5 +5,6 @@ porcent = float(input("What porcentage tip would you like to give? 10, 12, or 15
 people = int(input("How many people to split the bill? "))
 
 tax = (1 + porcent/100)
-person_pay = round((bill/people)*tax, 2)
-print(f"Each person should pay: ${person_pay}")
+person_pay = (bill/people)*tax
+final_amount = "{:.2f}".format(person_pay)
+print(f"Each person should pay: ${final_amount}")
