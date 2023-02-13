e22d8e989abd13f217304b6bf7e7a1e51269f926
Aline Watanabe <linenwatanabe@gmail.com>
2023-02-13 09:58:50 -0300

    New Notes

--- Day_02_Understanding_Data_Types_and_How_to_Manipulate_Strings/Notes.py
+++ Day_02_Understanding_Data_Types_and_How_to_Manipulate_Strings/Notes.py
@@ -1,4 +1,4 @@
-# Data Types
+"""Data Types"""
 
 # Strings
 print("Exemplos de Strings: ")
@@ -19,3 +19,55 @@ print(123.32)
 #Boolean
 True
 False
+
+num_char = len(input("What is your name?"))
+new_num_char = str(num_char)
+
+print("Your name has " + new_num_char + " characers")
+
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5"))
+print(str(70) + str(100))
+
+"""Mathematical Operation in Python"""
+
+3 + 5
+7 - 4
+3 * 2
+6 / 3
+2 ** 3
+
+# Ordem de Prioridade -> PEMDAS
+# 1 - Parentheses -> ()
+# 2 - Exponents -> **
+# 3 - Mutiplication -> *
+# 3 - Division -> /
+# 4 - Addition -> +
+# 4 - Subtraction -> -
+
+print(3 * 3 + 3 / 3 - 3)
+
+"""Number Manipulation and F Strings in Python"""
+
+print(8/3)
+print(int(8/3))
+print(round(8/3, 2))
+print(8//3)
+print(type(8//3))
+print(type(8/3))
+
+results = 4/2
+results /=2
+print(results)
+
+score = 0
+height = 1.8
+isWinning = True
+# User scores a point
+score += 1
+print("your score is " + str(score))
+
+#f-SyntaxWarning
+print(f"your score is {score}, your height is {height}, your are winning is {isWinning}")
