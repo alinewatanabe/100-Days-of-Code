e5e12897b3f7fb71ed8304cbecb5b8635baaa4ac
Aline Watanabe <linenwatanabe@gmail.com>
2023-02-13 08:15:24 -0300

    Day 02 - Add Notes

---
+++ Day_02_Understanding_Data_Types_and_How_to_Manipulate_Strings/Notes.py
@@ -0,0 +1,21 @@
+# Data Types
+
+# Strings
+print("Exemplos de Strings: ")
+print("Hello"[4])
+print("123" + "345")
+print("hello","world\n")
+
+#Integer
+print("Exemplos de Integer: ")
+print(123 + 345)
+print(123_456) # O python ignora o _ e considera um numero inteiro, isso é usado apenas para ajudar o programador a se organizar quando for mexer com mil, milhoes, bilhoes
+print(123_456 + 111)
+
+# Float
+print("\nExemplos de Float: ")
+print(123.32)
+
+#Boolean
+True
+False
