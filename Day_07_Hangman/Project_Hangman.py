24356f386d1d7380a0567c868a295f6504affc2f
Aline Watanabe <linenwatanabe@gmail.com>
2023-02-20 13:07:21 -0300

    Project - step 2_my_way

--- Day_07_Hangman/Project_Hangman.py
+++ Day_07_Hangman/Project_Hangman.py
@@ -1,13 +1,29 @@
 import random
 
 word_list = ["ardvark", "baboon", "camel"]
-
 word = random.choice(word_list)
+print(f"The answer is: {word}")
 
-guess = input("Guess a letter: ").lower()
+list = []
+list_conf = []
+
+print(len(word)*"_")
+
+guess = input("\nGuess a letter: ").lower()
 
 for letter in word:
     if(letter == guess):
+        ans = "Right"
         print("Right")
     else:
+        ans = "Wrong"
         print("Wrong")
+    list.append(ans)
+
+for answer in list:
+    if(answer == "Wrong"):
+        list_conf.append("_")
+    else:
+        list_conf.append(guess)
+
+print(list_conf)
