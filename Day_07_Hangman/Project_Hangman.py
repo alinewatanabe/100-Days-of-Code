b01c6a499b78638dbe9e39aa2ad6481b574d8b7f
Aline Watanabe <linenwatanabe@gmail.com>
2023-02-20 15:01:06 -0300

    Project - step 4

--- Day_07_Hangman/Project_Hangman.py
+++ Day_07_Hangman/Project_Hangman.py
@@ -1,5 +1,62 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["ardvark", "baboon", "camel"]
 word = random.choice(word_list)
 word_length = len(word)
@@ -8,20 +65,29 @@ print(f"The answer is: {word}")
 
 display = []
 end_of_game = False
+lives = 6
 
 for _ in range(word_length):
     display += "_"
-print(display)
 
 while (not end_of_game):
-    guess = input("Guess a letter: ").lower()
+    print(stages[lives])
+    guess = input("\nGuess a letter: ").lower()
     
     for position in range(word_length):
         letter = word[position]
         if(letter == guess):
-            display[position] = letter         
-    print(display)
+            display[position] = letter
+            
+    print(f"{' '.join(display)}")
     
     if "_" not in display:
         end_of_game = True
         print("You win!")
+
+    if guess not in word:
+        lives -= 1
+
+    if lives < 0:
+        end_of_game = True
+        print("You lose")
