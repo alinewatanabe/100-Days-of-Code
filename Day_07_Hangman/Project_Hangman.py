b0ce0766aec7925a14edf522177995ab52ca04de
Aline Watanabe <linenwatanabe@gmail.com>
2023-02-20 14:41:56 -0300

    Project - step 3

--- Day_07_Hangman/Project_Hangman.py
+++ Day_07_Hangman/Project_Hangman.py
@@ -7,15 +7,21 @@ word_length = len(word)
 print(f"The answer is: {word}")
 
 display = []
+end_of_game = False
 
 for _ in range(word_length):
     display += "_"
 print(display)
 
-guess = input("Guess a letter: ").lower()
-
-for position in range(word_length):
-    letter = word[position]
-    if(letter == guess):
-        display[position] = letter         
-print(display)
+while (not end_of_game):
+    guess = input("Guess a letter: ").lower()
+    
+    for position in range(word_length):
+        letter = word[position]
+        if(letter == guess):
+            display[position] = letter         
+    print(display)
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You win!")
