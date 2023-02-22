98875b8bf858d5661e384b03fada82988f390065
Aline Watanabe <linenwatanabe@gmail.com>
2023-02-22 08:33:07 -0300

    Project - ceaser fuction

--- Day_08_Function_Parameters_&_Caesar_Cipher/Project_Ceaser_Chiper.py
+++ Day_08_Function_Parameters_&_Caesar_Cipher/Project_Ceaser_Chiper.py
@@ -1,24 +1,27 @@
 import os
 import time
 
-def encrypt(plain_text, shift_amount):
-    cipher_txt = ""
-    for letter in plain_text:
+def ceaser(direction, text, shift):
+    txt = ""
+    for letter in text:
         position = alphabet.index(letter)
-        new_position = position + shift_amount
+        if(direction == "encode"):
+            new_position = position + shift
+            Error = False
+        elif(direction == "decode"):
+            new_position = position - shift
+            Error = False
+        else:
+            print("Error. Try Again")
+            time.sleep(2)
+            os.system("cls")
+            Error = True
         new_letter = alphabet[new_position]
-        cipher_txt += new_letter
-    print(f"The encode text is {cipher_txt}")
+        txt += new_letter
+    print(f"The {direction}d text is {txt}")
+    return Error
 
-def decrypt(cipher_text, shift_amount):
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        plain_text += new_letter
-    print(f"The decoded text is {plain_text}")
-
+        
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 Error = True
@@ -28,14 +31,4 @@ while(Error == True):
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))    
     
-    if(direction == "encode"):
-        encrypt(plain_text = text, shift_amount = shift)
-        Error = False
-    elif(direction == "decode"):
-        decrypt(cipher_text = text, shift_amount = shift)
-        Error = False
-    else:
-        print("Error. There isn't the option selected.\nTry Again!")
-        time.sleep(2)
-        os.system("cls")
-        Error = True
+    Error = ceaser(direction = direction, text = text, shift = shift)
