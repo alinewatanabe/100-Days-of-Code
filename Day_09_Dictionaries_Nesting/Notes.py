c8faa6a171b125a9057cf6f7d8d1abfdd2bfff75
Aline Watanabe <linenwatanabe@gmail.com>
2023-02-22 20:15:00 -0300

    Notes Add

--- Day_09_Dictionaries_Nesting/Notes.py
+++ Day_09_Dictionaries_Nesting/Notes.py
@@ -22,3 +22,36 @@ print(programming_dictionary)
 for key in programming_dictionary:
     print(key)
     print(programming_dictionary[key])
+
+"""Nesting Lists and Dictionaries"""
+
+capitals = {
+    "France" : "Paris",
+    "Germany" : "Berli,"
+}
+
+# Nesting a List in a Dictionary
+
+travel_log = {
+    "France" : {"cities_visited" : ["Paris", "Lille", "Dijon"], "total_visits" : 12},
+    "Germany": {"cities_visited" : ["Berlin", "Hamburg", "Stuttgart"], "total_visits" : 5}
+}
+
+for country in travel_log:
+    print(travel_log[country]["total_visits"])
+
+# Nesting Dictionary in a List
+travel_l = [
+    {
+     "country" : "France", 
+     "cities_visited" : ["Paris", "Lille", "Dijon"],
+     "total_visits" : 12
+    },
+    {
+     "country" : "Germany",
+     "cities_visited" : ["Berlin", "Hamburg", "Stuttgart"],
+     "total_visits" : 5
+    }
+]
+
+print(travel_l[0]["country"])
