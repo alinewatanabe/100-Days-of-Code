63fb6d4f5b538418d80d1dc7194d94be3e09e4fb
Aline Watanabe <linenwatanabe@gmail.com>
2023-02-22 08:08:40 -0300

    Project - decode

--- Day_08_Function_Parameters_&_Caesar_Cipher/Project_Ceaser_Chiper.py
+++ Day_08_Function_Parameters_&_Caesar_Cipher/Project_Ceaser_Chiper.py
@@ -1,8 +1,5 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+import os
+import time
 
 def encrypt(plain_text, shift_amount):
     cipher_txt = ""
@@ -13,4 +10,32 @@ def encrypt(plain_text, shift_amount):
         cipher_txt += new_letter
     print(f"The encode text is {cipher_txt}")
 
-encrypt(plain_text = text, shift_amount = shift)
+def decrypt(cipher_text, shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        plain_text += new_letter
+    print(f"The decoded text is {plain_text}")
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+Error = True
+
+while(Error == True):
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))    
+    
+    if(direction == "encode"):
+        encrypt(plain_text = text, shift_amount = shift)
+        Error = False
+    elif(direction == "decode"):
+        decrypt(cipher_text = text, shift_amount = shift)
+        Error = False
+    else:
+        print("Error. There isn't the option selected.\nTry Again!")
+        time.sleep(2)
+        os.system("cls")
+        Error = True
