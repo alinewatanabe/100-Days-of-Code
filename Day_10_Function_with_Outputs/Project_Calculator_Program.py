b1b0606ec157ea082d38cc3e0ea9cc26b8aea5f4
Aline Watanabe <linenwatanabe@gmail.com>
2023-02-22 23:42:50 -0300

    Project Calculator Program Final Version

--- Day_10_Function_with_Outputs/Project_Calculator_Program.py
+++ Day_10_Function_with_Outputs/Project_Calculator_Program.py
@@ -1,3 +1,6 @@
+from Art_Project import logo
+from os import system
+
 def add(n1, n2):
     return n1 + n2
 
@@ -17,8 +20,8 @@ operations = {
     "/" : divide
 }
 def calculator():
-    
-    num1 = int(input("What's the first number? "))
+    print(logo)
+    num1 = float(input("What's the first number? "))
     
     for symbol in operations:
         print(symbol)
@@ -27,15 +30,19 @@ def calculator():
     
     while (should_continue):
         operation_symbol = input("Pick an operation: ")
-        num2 = int(input("What's the next number? "))   
+        num2 = float(input("What's the next number? "))   
         answer = operations[operation_symbol](num1, num2)
         
         print(f"{num1} {operation_symbol} {num2} = {answer}")
         
         if (input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ").lower() == "y"):
             num1 = answer
+            system("cls")
+            print(logo)
+            print(f"Number 1: {num1}")
         else:
             should_continue = False
+            system("cls")
             calculator()
 
 calculator()
