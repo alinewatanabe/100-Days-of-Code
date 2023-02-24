d0b9db07509f6bce26a97b4fedc189e088e1c604
Aline Watanabe <linenwatanabe@gmail.com>
2023-02-24 09:15:36 -0300

    Loop

--- Day_11_The_Blackjack_Capstone_Project/Project_Blackjack.py
+++ Day_11_The_Blackjack_Capstone_Project/Project_Blackjack.py
@@ -5,6 +5,12 @@ def add_card(view_list, cards, user_list, symbol_cards):
     position = cards.index(value_random)
     user_list.append(value_random)
     view_list.append(symbol_cards[position])
+    
+def score(base_list):
+    score = 0
+    for number in base_list:
+        score += number
+    return score
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 symbol_cards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
@@ -12,6 +18,7 @@ user_list = []
 view_user_list = []
 computer_list = []
 view_comp_list = []
+another_card = 'y'
 
 # Add Initial User Cards
 add_card(view_user_list, cards, user_list, symbol_cards)
@@ -21,8 +28,17 @@ add_card(view_user_list, cards, user_list, symbol_cards)
 add_card(view_comp_list, cards, computer_list, symbol_cards)
 add_card(view_comp_list, cards, computer_list, symbol_cards)
 
-score = 0
-for number in user_list:
-    score += number
-    
-print(f"Your cards: {view_user_list}, current score: {score}")
+while(another_card == 'y'):
+    # Scores
+    user_score = score(user_list)
+    comp_score = score(computer_list)
+        
+    print(f"Your cards: {view_user_list}, current score: {user_score}")
+        
+    #print(f"Computer's cars: {view_comp_list}, current score: {comp_score}")
+    print(f"Computer's first card: {view_comp_list[0]}")
+
+    another_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+
+    if (another_card == 'y'):
+        add_card(view_user_list, cards, user_list, symbol_cards)
