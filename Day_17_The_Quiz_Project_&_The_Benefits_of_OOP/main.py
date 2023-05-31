f875e17ca3748858c18e747ac29b12b4501341c5
Aline Watanabe <linenwatanabe@gmail.com>
2023-05-31 20:13:03 -0300

    main complete

--- Day_17_The_Quiz_Project_&_The_Benefits_of_OOP/main.py
+++ Day_17_The_Quiz_Project_&_The_Benefits_of_OOP/main.py
@@ -1,6 +1,6 @@
 class User:
-    def __init__(self, user_id, username):
-        self.id = user_id
+    def __init__(self, id, username):
+        self.id = id
         self.username = username
         self.followers = 0
         self.following = 0
@@ -9,12 +9,14 @@ class User:
         user.followers += 1
         self.following += 1
 
-user_1 = User("001", "Aline")
+# user_2 = User()
+# user_2.id = "002"
+# user_2.username = "Yugo"
+
+user_1 = User("001", "Lin")
 user_2 = User("002", "Yugo")
 
 user_1.follow(user_2)
 
-print(user_1.followers)
 print(user_1.following)
 print(user_2.followers)
-print(user_2.following)
